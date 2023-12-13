5e70ef5c55866e73b248e9156442073fb9fc1ffa
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2023-12-13 12:18:24 +0400

    added backtoprods button in create

--- create.php
+++ create.php
@@ -79,6 +79,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
+    <p> 
+    <a href="index.php" class="btn btn-secondary"> Back to products </a>
+    </p>
     <h1>Create new Product</h1>
 
 <?php if (!empty($errors)): ?>
