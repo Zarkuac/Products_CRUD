ed3ac6f9277bdd0506a2115e3b99ee04b719b82f
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-01-29 14:08:51 +0400

    search functional finished

--- public/index.php
+++ public/index.php
@@ -2,7 +2,15 @@
 
   require_once '../database.php';
 
-$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+  $keyword = $_GET['search'] ?? null;
+  if ($keyword) {
+    $statement = $pdo->prepare('SELECT * FROM products WHERE title like :keyword ORDER BY create_date DESC');
+    $statement->bindValue(":keyword", "%$keyword%");
+}
+ else {
+  $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+}
+ 
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -13,16 +21,13 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     <a href="create.php" type="button" class="btn btn-sm btn-success">Add Product</a>
     </p>
 
-
     <form action="" method="get">
     <div class="input-group mb-3">
-      <input type="text" name="search" class="form-control" placeholder="Search">
-      <div class="input-group-append">
+    <input type="text" name="search" class="form-control" placeholder="Search" value="<?php echo $keyword ?>">      <div class="input-group-append">
         <button class="btn btn-success" type="submit">Search</button>
       </div>
     </div>
 </form>
-
 
     <table class="table">
   <thead>
